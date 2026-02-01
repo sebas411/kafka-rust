93c4929eb947dc0a627c4d1c54b1879e775949f1
sebas411 <sebasma411@gmail.com>
2026-02-01 11:35:01 -0600

    Parse API Version

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,28 @@ fn main() -> Result<()> {
                 let mut buffer = [0u8; 1024];
                 stream.read(&mut buffer)?;
                 let _request_size = i32::from_be_bytes(buffer[..4].try_into()?);
+                let request_api_key = i16::from_be_bytes(buffer[4..6].try_into()?);
+                let request_api_version = i16::from_be_bytes(buffer[6..8].try_into()?);
                 let correlation_id = i32::from_be_bytes(buffer[8..12].try_into()?);
 
                 let mut response = vec![];
                 let response_size = 0i32;
+                let mut response_body = vec![];
+                match request_api_key {
+                    18 =>  {
+                        let error_code: i16;
+                        if request_api_version > 4 || request_api_version < 0 {
+                            error_code = 35;
+                        } else {
+                            error_code = 0;
+                        }
+                        response_body.extend(error_code.to_be_bytes());
+                    },
+                    _ => ()
+                }
                 response.extend(response_size.to_be_bytes());
                 response.extend(correlation_id.to_be_bytes());
+                response.extend(response_body);
                 stream.write_all(&response)?;
             }
             Err(e) => {
