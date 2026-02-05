6aea2d88f62c5de3fcb88b8683359f91a1a9ce8f
sebas411 <sebasma411@gmail.com>
2026-02-05 13:05:41 -0600

    handle apiversions request

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
 use std::{io::{Read, Write}, net::TcpListener};
 use anyhow::Result;
 
+struct ApiVersion {
+    api_key: i16,
+    min_version: i16,
+    max_version: i16,
+}
+
+impl ApiVersion {
+    fn new(api_key: i16, min_version: i16, max_version: i16) -> Self {
+        Self { api_key, min_version, max_version }
+    }
+    fn encode(&self) -> Vec<u8> {
+        let mut encoded_bytes = vec![];
+        encoded_bytes.extend(self.api_key.to_be_bytes());
+        encoded_bytes.extend(self.min_version.to_be_bytes());
+        encoded_bytes.extend(self.max_version.to_be_bytes());
+        encoded_bytes
+    }
+}
+
+fn generate_api_versions() -> Vec<ApiVersion> {
+    let mut versions = vec![];
+    versions.push(ApiVersion::new(18, 0, 4)); //ApiVersions
+    versions
+}
+
 fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
     println!("Listening on {}", listener.local_addr().unwrap().to_string());
+
+    let apiversions = generate_api_versions();
     
     for stream in listener.incoming() {
         match stream {
@@ -17,7 +44,6 @@ fn main() -> Result<()> {
                 let correlation_id = i32::from_be_bytes(buffer[8..12].try_into()?);
 
                 let mut response = vec![];
-                let response_size = 0i32;
                 let mut response_body = vec![];
                 match request_api_key {
                     18 =>  {
@@ -28,9 +54,17 @@ fn main() -> Result<()> {
                             error_code = 0;
                         }
                         response_body.extend(error_code.to_be_bytes());
+                        response_body.extend(((apiversions.len() + 1) as i8).to_be_bytes());
+                        for api_version in &apiversions {
+                            response_body.extend(api_version.encode());
+                        }
+                        response_body.push(0); // tag
+                        response_body.extend(0i32.to_be_bytes()); // throttle time
+                        response_body.push(0); // tag
                     },
                     _ => ()
                 }
+                let response_size = 4 + response_body.len() as i32;
                 response.extend(response_size.to_be_bytes());
                 response.extend(correlation_id.to_be_bytes());
                 response.extend(response_body);
