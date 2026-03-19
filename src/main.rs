2cd1503c82f7719f6dc2590741c8b69e86f0e5b8
sebas411 <sebasma411@gmail.com>
2026-03-19 14:18:37 -0600

    Refactor client handler code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::{collections::HashMap};
 use anyhow::Result;
-use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::{TcpListener, TcpStream}};
+use tokio::net::TcpListener;
 
-use crate::modules::value::{Topic, compact_array_encode};
+use crate::modules::client_handler::handle_client;
 mod modules;
 
 #[derive(Debug, Clone)]
@@ -30,84 +30,6 @@ fn generate_api_versions() -> HashMap<i16, ApiVersion> {
     versions.insert(18, ApiVersion::new(18, 0, 4)); // ApiVersions
     versions.insert(75, ApiVersion::new(75, 0, 0)); // DescribeTopicPartitions
     versions
-}
-
-async fn handle_client(mut stream: TcpStream, apiversions: HashMap<i16, ApiVersion>) -> Result<()> {
-    println!("Accepted new connection from {}", stream.peer_addr().unwrap().to_string());
-    let mut buffer = [0u8; 10*1024]; // 10KiB buffer for request
-
-    loop {
-        stream.read(&mut buffer).await?;
-        let _request_size = i32::from_be_bytes(buffer[..4].try_into()?);
-        let request_api_key = i16::from_be_bytes(buffer[4..6].try_into()?);
-        let request_api_version = i16::from_be_bytes(buffer[6..8].try_into()?);
-        let correlation_id = i32::from_be_bytes(buffer[8..12].try_into()?);
-        let mut response_version= 0;
-    
-        let mut response = vec![];
-        let mut response_body = vec![];
-    
-        let error_code: i16;
-        if let Some(api_version) = apiversions.get(&request_api_key) && request_api_version >= api_version.min_version && request_api_version <= api_version.max_version {
-            error_code = 0
-        } else {
-            error_code = 35
-        }
-    
-        if error_code == 0 {
-            match request_api_key {
-                18 =>  { // ApiVersions
-                    response_body.extend(error_code.to_be_bytes());
-                    // array length
-                    response_body.extend(((apiversions.len() + 1) as i8).to_be_bytes());
-                    // api versions
-                    for (_, api_version) in &apiversions {
-                        response_body.extend(api_version.encode()); // key, min, max
-                        response_body.push(0); // tag
-                    }
-                    response_body.extend(0i32.to_be_bytes()); // throttle time
-                    response_body.push(0); // tag
-                },
-                75 => { // DescribeTopicPartitions
-                    let mut cursor = 12;
-                    let client_id_length = u16::from_be_bytes(buffer[cursor .. cursor + 2].try_into()?) as usize;
-                    let _client_id = String::from_utf8(buffer[cursor + 2 .. cursor + 2 + client_id_length].to_vec())?;
-                    cursor += 2 + client_id_length + 1; // c_id_length + c_id + empty tag buffer
-
-                    response_version = 1;
-                    let throttle_time = 0i32;
-                    let topics_requested = buffer[cursor] as usize - 1;
-                    cursor += 1;
-                    let mut topics = vec![];
-                    for _ in 0..topics_requested {
-                        let topic_name_length = buffer[cursor] as usize - 1;
-                        let topic_name = String::from_utf8(buffer[cursor + 1 .. cursor + 1 + topic_name_length].to_vec())?;
-                        let topic = Topic::new(3, &topic_name, [0u8; 16], false, vec![], 0);
-                        topics.push(topic);
-                        cursor += 1 + topic_name_length + 1; // name_length + name + empty tag buffer
-                    }
-
-                    response_body.extend(throttle_time.to_be_bytes()); // throttle time
-                    response_body.extend(compact_array_encode(&topics));
-                    response_body.extend((-1i8).to_be_bytes()); // null next cursor
-                    response_body.push(0); // tags
-                },
-                _ => ()
-            }
-        } else {
-            response_body.extend(error_code.to_be_bytes());
-        }
-        
-        let mut response_size = 4 + response_body.len() as i32;
-        if response_version == 1 {
-            response_size += 1;
-            response_body.insert(0, 0);
-        }
-        response.extend(response_size.to_be_bytes());
-        response.extend(correlation_id.to_be_bytes());
-        response.extend(response_body);
-        stream.write_all(&response).await?;
-    }
 }
 
 #[tokio::main()]
